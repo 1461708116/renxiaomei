d52699f72261f751fce81ddc58704ab26d217d28
146708116 <1461708116@qq.com>
2017-03-05 21:54:46 +0800

    renxioamei

---
+++ java/renxiaomei/src/main/java/com/renxiaomei/RenxioameiTest.java
@@ -0,0 +1,14 @@
+package com.renxiaomei;
+
+import java.util.Vector;
+
+/**
+ * Created by renxiaomei on 2017/3/5.
+ */
+public class RenxioameiTest {
+    public static void main(String[] args){
+        Vector v = new Vector();
+        String str = "jjj0";
+        System.out.print(str);
+    }
+}
